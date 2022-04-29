7456dc6e4b9734103895285282782c739334251b
Crispthofer A. Rincon M <alex877@gmail.com>
2022-04-28 22:49:21 -0500

    [Update] onBlur property was added to manage visited fileds\n[Add] Checked Vaccinated was added to activate the inputs of Vaccine information

--- src/components/CrudForm.js
+++ src/components/CrudForm.js
@@ -6,23 +6,24 @@ import { useFormik } from "formik";
 // which keys are symmetrical to our values/initialValues
 const validate = values => {
   const errors = {};
+
   // id citizen validation
   if (!values.idcitizen) {
     errors.idcitizen = 'Required';
-  } else if (values.idcitizen.toString().lenght < 8 ||
-            values.idcitizen.toString().lenght > 10) {
-    errors.idcitizen = 'Lenght of Id citizen is wrong';
+  } else if (values.idcitizen.toString().length < 8 ||
+            values.idcitizen.toString().length > 10) {
+    errors.idcitizen = 'Id citizen is not correct';
   }
   // Name validation
   if (!values.name) {
     errors.name = 'Required';
-  } else if (values.name.lenght > 15) {
+  } else if (values.name.length > 15) {
     errors.name = 'Must be 15 characters or less';
   }
   // Last Name validation
   if (!values.lastname) {
     errors.lastname = 'Required';
-  } else if (values.lastname.lenght > 15) {
+  } else if (values.lastname.length > 15) {
     errors.lastname = 'Must be 15 characters or less';
   }
   // Email validation
@@ -38,15 +39,15 @@ const validate = values => {
   // Address validation
   if (!values.address) {
     errors.address = 'Required';
-  } else if (values.address.lenght < 10) {
+  } else if (values.address.length < 20) {
     errors.address = 'Must be 10 characters or more';
   }
   // Phone validation
   if (!values.phone) {
     errors.phone = 'Required';
-  } else if (values.phone.toString().lenght < 6 ||
-            values.phone.toString().lenght > 15) {
-    errors.phone = 'Lenght of phone is wrong';
+  } else if (values.phone.toString().length < 6 ||
+            values.phone.toString().length > 15) {
+    errors.phone = 'length of phone is wrong';
   }
   /*
     Vaccine information validation
@@ -65,8 +66,8 @@ const validate = values => {
       errors.vaccinedose = 'Required';
     }
   }
-
-}
+  return errors;
+};
 
 // Employyee Vaccinate Inventory form
 const CrudForm = () => {
@@ -87,13 +88,18 @@ const CrudForm = () => {
       vaccinedate: '',
       vaccinedose: '',
     },
+    validate,
     onsubmit: values => {
       alert(JSON.stringify(values, null, 2));
     },
   });
   /**
-    Adding messsage to the user on the User form
-    {formik.errors.idcitizen ? <div>{formik.errors.idcitizen}</div> : null} 
+    Error Messsage to the user on the User form
+    {formik.errors.idcitizen ? <div>{formik.errors.idcitizen}</div> : null}
+
+    Visited fields
+    formik.handleBlur: To take advantage of touched, it passes to each
+    input’s onBlur prop
   */
   return (
     <form onSubmit={formik.handleSubmit}>
@@ -103,10 +109,13 @@ const CrudForm = () => {
          name="idcitizen"
          type="number"
          onChange={formik.handleChange}
-         value={formik.values.cc}
+         onBlur={formik.handleBlur}
+         value={formik.values.idcitizen}
          placeholder="123456789"
       />
-      {formik.errors.idcitizen ? <div>{formik.errors.idcitizen}</div> : null}
+      {formik.touched.idcitizen && formik.errors.idcitizen ? (
+        <div>{formik.errors.idcitizen}</div>
+      ) : null}
       <br/>
       <label htmlFor="name"> Name </label>
       <input
@@ -114,10 +123,13 @@ const CrudForm = () => {
         name="name"
         type="text"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.name}
         placeholder="Name Employee"
       />
-      {formik.errors.name ? <div>{formik.errors.name}</div> : null}
+      {formik.touched.name && formik.errors.name ? (
+        <div>{formik.errors.name}</div>
+      ) : null}
       <br/>
       <label htmlFor="lastname"> Lastname </label>
       <input
@@ -125,10 +137,13 @@ const CrudForm = () => {
         name="lastname"
         type="text"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.lastname}
         placeholder="LastName Employee"
       />
-      {formik.errors.lastname ? <div>{formik.errors.lastname}</div> : null}
+      {formik.touched.lastname && formik.errors.lastname ? (
+        <div>{formik.errors.lastname}</div>
+      ) : null}
       <br/>
       <label htmlFor="email"> Email Address </label>
       <input
@@ -136,10 +151,13 @@ const CrudForm = () => {
         name="email"
         type="email"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.email}
         placeholder="jhonydeeptrust@gmail.com"
       />
-      {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+      {formik.touched.email && formik.errors.email ? (
+        <div>{formik.errors.email}</div>
+      ) : null}
       <br/>
       <label htmlFor="birthday"> Birthday </label>
       <input
@@ -147,11 +165,14 @@ const CrudForm = () => {
         name="birthday"
         type="date"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.birthday}
         min="1950-01-01"
         max="2004-01-01"
       />
-      {formik.errors.birthday ? <div>{formik.errors.birthday}</div> : null}
+      {formik.touched.birthday && formik.errors.birthday ? (
+        <div>{formik.errors.birthday}</div>
+      ) : null}
       <br/>
       <label htmlFor="address"> Home Address </label>
       <input
@@ -159,10 +180,13 @@ const CrudForm = () => {
         name="address"
         type="text"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.address}
         placeholder="Av. July 20 Street 85 House 23"
       />
-      {formik.errors.address ? <div>{formik.errors.address}</div> : null}
+      {formik.touched.address && formik.errors.address ? (
+        <div>{formik.errors.address}</div>
+      ) : null}
       <br/>
       <label htmlFor="phone"> Phone </label>
       <input
@@ -170,10 +194,13 @@ const CrudForm = () => {
         name="phone"
         type="tel"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.phone}
         placeholder="+593 678456309"
       />
-      {formik.errors.phone ? <div>{formik.errors.phone}</div> : null}
+      {formik.touched.phone && formik.errors.phone ? (
+        <div>{formik.errors.phone}</div>
+      ) : null}
       <br/>
       <label htmlFor="checkvaccinated">
         <input
@@ -181,51 +208,66 @@ const CrudForm = () => {
           name="checkvaccinated"
           type="checkbox"
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
           value={formik.values.checkvaccinated}
         />
         COVID vaccinated
       </label>
       <br/>
-      
-      <label htmlFor="vaccinename"> Vaccine name </label>
-      <select
-        id="vaccinename"
-        name="vaccinename"
-        typeof="text"
-        onChange={formik.handleChange}
-        value={formik.values.vaccinename}
-      >
-        <option value="Sputnik">Sputnik</option>
-        <option value="AstraZeneca">AstraZeneca</option>
-        <option value="Pfizer">Pfizer</option>
-        <option value="Jhonson&Jhonson">Jhonson&Jhonson</option>
-      </select>
-      {formik.errors.vaccinename ? <div>{formik.errors.vaccinename}</div> : null}
-      <br/>
-      <label htmlFor="vaccinedate"> Vaccinated date </label>
-      <input
-        id="vaccinedate"
-        name="vaccinedate"
-        type="date"
-        onChange={formik.handleChange}
-        value={formik.values.vaccinedate}
-        min="2020-01-01"
-      />
-      {formik.errors.vaccinedate ? <div>{formik.errors.vaccinedate}</div> : null}
-      <br/>
-      <label htmlFor="vaccinedose"> Vaccine dose </label>
-      <select
-        id="vaccinedose"
-        name="vaccinedose"
-        typeof="number"
-        onChange={formik.handleChange}
-        value={formik.values.vaccinedose}
-      >
-        <option value="dose1">1</option>
-        <option value="dose2">2</option>
-        <option value="dose3">3</option>
-      </select>
-      {formik.errors.vaccinedose ? <div>{formik.errors.vaccinedose}</div> : null}
+      <div className='infoVaccinated' style={formik.values.checkvaccinated ?
+        {display: "block"} :
+        {display: "none"}
+      }>
+
+        <label htmlFor="vaccinename"> Vaccine name </label>
+        <select
+          id="vaccinename"
+          name="vaccinename"
+          typeof="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.checkvaccinated ? formik.values.vaccinename : ''}
+        >
+          <option value="Sputnik">Sputnik</option>
+          <option value="AstraZeneca">AstraZeneca</option>
+          <option value="Pfizer">Pfizer</option>
+          <option value="Jhonson&Jhonson">Jhonson&Jhonson</option>
+        </select>
+        {formik.touched.vaccinename && formik.errors.vaccinename ? (
+          <div>{formik.errors.vaccinename}</div>
+        ) : null}
+        <br/>
+        <label htmlFor="vaccinedate"> Vaccinated date </label>
+        <input
+          id="vaccinedate"
+          name="vaccinedate"
+          type="date"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.checkvaccinated ? formik.values.vaccinedate : ''}
+          min="2020-01-01"
+        />
+        {formik.touched.vaccinedate && formik.errors.vaccinedate ? (
+          <div>{formik.errors.vaccinedate}</div>
+        ) : null}
+        <br/>
+        <label htmlFor="vaccinedose"> Vaccine dose </label>
+        <select
+          id="vaccinedose"
+          name="vaccinedose"
+          typeof="number"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.checkvaccinated ? formik.values.vaccinedose : ''}
+        >
+          <option value="dose1">1</option>
+          <option value="dose2">2</option>
+          <option value="dose3">3</option>
+        </select>
+        {formik.touched.vaccinedose && formik.errors.vaccinedose ? (
+          <div>{formik.errors.vaccinedose}</div>
+        ) : null}
+      </div>
       <br/>
 
       <button type="submit">Submit</button>
