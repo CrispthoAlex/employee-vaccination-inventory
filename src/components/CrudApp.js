eb64847776c80b1ccd1bad6efc225ffd667d9ea9
Crispthofer A. Rincon M <alex877@gmail.com>
2022-04-29 12:29:50 -0500

    [Update] CrudForm and CrudTable components were added\n[Update] initialDb (fake DB) was updated

--- src/components/CrudApp.js
+++ src/components/CrudApp.js
@@ -1,63 +1,62 @@
-import React from 'react'
+import React, {useState} from 'react';
 import CrudForm from './CrudForm';
+import CrudTable from './CrudTable';
+
 
 // DB initial
 const initialDb = [
     {
         id: 1,
-        cc: 123456789,
+        idcitizen: 123456789,
         name: "Lucy",
         lastname: "Fihum",
         email: "lucy.fihum@gmail.com",
         birthday: null,
         address: null,
         phone: null,
-        vaccinated: null,
-        infovaccine: {
-            name: null, 
-            date: null,
-            dose: 0,
-        }
+        vaccinated: false,
+        vaccinename: null,
+        vaccinedate: null,
+        vaccinedose: 0,
     },
     {
         id: 2,
-        cc: 234567890,
+        idcitizen: 234567890,
         name: "Peter",
         lastname: "Parker",
         email: "man.spider@gmail.com",
         birthday: null,
         address: null,
         phone: null,
-        vaccinated: null,
-        infovaccine: {
-            name: null, 
-            date: null,
-            dose: 0,
-        }
+        vaccinated: false,
+        vaccinename: null,
+        vaccinedate: null,
+        vaccinedose: 0,
     },
     {
         id: 3,
-        cc: 345678901,
+        idcitizen: 345678901,
         name: "Josh",
         lastname: "Brolin",
         email: "angry.hulk@gmail.com",
         birthday: null,
         address: null,
         phone: null,
-        vaccinated: null,
-        infovaccine: {
-            name: null, 
-            date: null,
-            dose: 0,
-        }
+        vaccinated: false,
+        vaccinename: null,
+        vaccinedate: null,
+        vaccinedose: 0,
     }
 ]
 
 const CrudApp = () => {
+  const [db, setDb] = useState(initialDb);
+
   return (
     <div>
         <h2>MANAGE USERS</h2>
         <CrudForm />
+        <CrudTable data={db}/>
     </div>
   );
 }
