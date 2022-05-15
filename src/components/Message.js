c35e4fcc4d7a38c28df7ebcb02c585350d9afd42
Crispthofer A. Rincon M <alex877@gmail.com>
2022-05-14 21:45:30 -0500

    [Add] Message component was created with inline styles

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const Message = () => {
+const Message = ({errorMsg, bgColor}) => {
+  // Adding inline styles to the Error message
+  let stylesMsg = {
+    padding: "1rem",
+    marginBottom: "1.5rem",
+    textAlign: "center",
+    color: "#fff",
+    fontWeight: "bold",
+    backgroundColor: bgColor,
+  };
   return (
-    <div>
-        <h2>Message</h2>
+    <div style={stylesMsg}>
+      <p>{errorMsg}</p>
     </div>
   )
 }
